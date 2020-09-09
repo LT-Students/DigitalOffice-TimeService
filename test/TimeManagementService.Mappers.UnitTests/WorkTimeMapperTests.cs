5e444fb24bba5dbcaeda9bab56f6e6bc45322d01
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-09-09 17:20:11 +0300

    Updated tests

--- test/TimeManagementService.Mappers.UnitTests/WorkTimeMapperTests.cs
+++ test/TimeManagementService.Mappers.UnitTests/WorkTimeMapperTests.cs
@@ -9,21 +9,24 @@ namespace LT.DigitalOffice.TimeManagementService.Mappers.UnitTests
 {
     public class WorkTimeMapperTests
     {
-        private IMapper<CreateWorkTimeRequest, DbWorkTime> mapper;
+        private IMapper<CreateWorkTimeRequest, DbWorkTime> createRequestMapper;
+        private IMapper<EditWorkTimeRequest, DbWorkTime> editRequestMapper;
 
-        private CreateWorkTimeRequest request;
+        private CreateWorkTimeRequest createRequest;
+        private EditWorkTimeRequest editRequest;
         private DbWorkTime expectedDbWorkTimeWithoutId;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
-            mapper = new WorkTimeMapper();
+            createRequestMapper = new WorkTimeMapper();
+            editRequestMapper = new WorkTimeMapper();
         }
 
         [SetUp]
         public void SetUp()
         {
-            request = new CreateWorkTimeRequest
+            createRequest = new CreateWorkTimeRequest
             {
                 ProjectId = Guid.NewGuid(),
                 StartTime = new DateTime(2020, 7, 29, 9, 0, 0),
@@ -33,27 +36,39 @@ namespace LT.DigitalOffice.TimeManagementService.Mappers.UnitTests
                 WorkerUserId = Guid.NewGuid()
             };
 
+            editRequest = new EditWorkTimeRequest
+            {
+                Id = Guid.NewGuid(),
+                ProjectId = createRequest.ProjectId,
+                StartTime = createRequest.StartTime,
+                EndTime = createRequest.EndTime,
+                Title = createRequest.Title,
+                Description = createRequest.Description,
+                WorkerUserId = createRequest.WorkerUserId
+            };
+
             expectedDbWorkTimeWithoutId = new DbWorkTime
             {
-                ProjectId = request.ProjectId,
-                StartTime = request.StartTime,
-                EndTime = request.EndTime,
-                Title = request.Title,
-                Description = request.Description,
-                WorkerUserId = request.WorkerUserId
+                ProjectId = createRequest.ProjectId,
+                StartTime = createRequest.StartTime,
+                EndTime = createRequest.EndTime,
+                Title = createRequest.Title,
+                Description = createRequest.Description,
+                WorkerUserId = createRequest.WorkerUserId
             };
         }
 
+        #region CreateWorkTimeRequest to DbWorkTime
         [Test]
         public void ShouldThrowArgumentNullExceptionWhenCreateWorkTimeRequestIsNull()
         {
-            Assert.Throws<ArgumentNullException>(() => mapper.Map(null));
+            Assert.Throws<ArgumentNullException>(() => createRequestMapper.Map(null));
         }
 
         [Test]
         public void ShouldReturnDbWorkTimeWhenMappingValidCreateWorkTimeRequest()
         {
-            var newWorkTime = mapper.Map(request);
+            var newWorkTime = createRequestMapper.Map(createRequest);
             expectedDbWorkTimeWithoutId.Id = newWorkTime.Id;
 
             Assert.IsInstanceOf<Guid>(newWorkTime.Id);
@@ -63,15 +78,33 @@ namespace LT.DigitalOffice.TimeManagementService.Mappers.UnitTests
         [Test]
         public void ShouldReturnDbWorkTimeWhenMappingValidCreateWorkTimeRequestWithNullDescription()
         {
-            request.Description = null;
+            createRequest.Description = null;
             expectedDbWorkTimeWithoutId.Description = null;
 
-            var newWortTime = mapper.Map(request);
+            var newWortTime = createRequestMapper.Map(createRequest);
             expectedDbWorkTimeWithoutId.Id = newWortTime.Id;
 
             Assert.IsInstanceOf<Guid>(newWortTime.Id);
             Assert.IsTrue(string.IsNullOrEmpty(newWortTime.Description));
             SerializerAssert.AreEqual(expectedDbWorkTimeWithoutId, newWortTime);
         }
+        #endregion
+
+        #region EditWorkTimeRequest to DbWorkTime
+        [Test]
+        public void ShouldThrowArgumentNullExceptionWhenEditWorkTimeRequestIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => editRequestMapper.Map(null));
+        }
+
+        [Test]
+        public void ShouldReturnDbWorkTimeWhenMappingValidEditWorkTimeRequest()
+        {
+            var workTime = editRequestMapper.Map(editRequest);
+            expectedDbWorkTimeWithoutId.Id = editRequest.Id;
+
+            SerializerAssert.AreEqual(expectedDbWorkTimeWithoutId, workTime);
+        }
+        #endregion
     }
 }
