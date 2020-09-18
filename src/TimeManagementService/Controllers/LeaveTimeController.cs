f9580707ddc618598c862233a7dc13ab3037f247
djino <malkinevgeniy437@gmail.com>
2020-09-19 00:04:05 +0300

    Added LeaveTimeController

---
+++ src/TimeManagementService/Controllers/LeaveTimeController.cs
@@ -0,0 +1,18 @@
+﻿using LT.DigitalOffice.TimeManagementService.Business.Interfaces;
+using LT.DigitalOffice.TimeManagementService.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LT.DigitalOffice.TimeManagementService.Controllers
+{
+    public class LeaveTimeController
+    {
+        [HttpPost("addLeaveTime")]
+        public Guid AddLeaveTime(
+            [FromBody] CreateLeaveTimeRequest leaveTime,
+            [FromServices] ICreateLeaveTimeCommand command)
+        {
+            return command.Execute(leaveTime);
+        }
+    }
+}
