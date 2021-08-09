9efdcad82784c5c41dc944b9ac62c2400bdb87c1
BelehovEgor <belehov.egor2001@yandex.ru>
2021-08-09 11:06:42 +0300

    fix edit

--- src/TimeService.Validation/WorkTime/EditWorkTimeRequestValidator.cs
+++ src/TimeService.Validation/WorkTime/EditWorkTimeRequestValidator.cs
@@ -4,9 +4,7 @@ using LT.DigitalOffice.TimeService.Models.Dto.Requests;
 using LT.DigitalOffice.TimeService.Validation.Helper;
 using LT.DigitalOffice.TimeService.Validation.WorkTime.Interfaces;
 using Microsoft.AspNetCore.JsonPatch.Operations;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace LT.DigitalOffice.TimeService.Validation.WorkTime
 {
@@ -22,80 +20,54 @@ namespace LT.DigitalOffice.TimeService.Validation.WorkTime
             AddСorrectPaths(
                 new()
                 {
-                    nameof(EditWorkTimeRequest.StartTime),
-                    nameof(EditWorkTimeRequest.EndTime),
-                    nameof(EditWorkTimeRequest.Minutes),
-                    nameof(EditWorkTimeRequest.Title),
-                    nameof(EditWorkTimeRequest.IsActive),
-                    nameof(EditWorkTimeRequest.ProjectId),
+                    nameof(EditWorkTimeRequest.Day),
+                    nameof(EditWorkTimeRequest.UserHours),
+                    nameof(EditWorkTimeRequest.ManagerHours),
                     nameof(EditWorkTimeRequest.Description)
                 });
 
-            AddСorrectOperations(nameof(EditWorkTimeRequest.StartTime), new List<OperationType> { OperationType.Replace });
-            AddСorrectOperations(nameof(EditWorkTimeRequest.EndTime), new List<OperationType> { OperationType.Replace });
-            AddСorrectOperations(nameof(EditWorkTimeRequest.Minutes), new List<OperationType> { OperationType.Replace });
-            AddСorrectOperations(nameof(EditWorkTimeRequest.Title), new List<OperationType> { OperationType.Replace });
-            AddСorrectOperations(nameof(EditWorkTimeRequest.IsActive), new List<OperationType> { OperationType.Replace });
+            AddСorrectOperations(nameof(EditWorkTimeRequest.Day), new List<OperationType> { OperationType.Replace });
+            AddСorrectOperations(nameof(EditWorkTimeRequest.UserHours), new List<OperationType> { OperationType.Replace });
+            AddСorrectOperations(nameof(EditWorkTimeRequest.ManagerHours), new List<OperationType> { OperationType.Replace });
             AddСorrectOperations(nameof(EditWorkTimeRequest.Description), new List<OperationType> { OperationType.Replace });
-            AddСorrectOperations(nameof(EditWorkTimeRequest.ProjectId), new List<OperationType> { OperationType.Replace });
             #endregion
 
-            #region StartTime
+            #region Day
 
             AddFailureForPropertyIf(
-                nameof(EditWorkTimeRequest.StartTime),
+                nameof(EditWorkTimeRequest.Day),
                 x => x == OperationType.Replace,
                 new()
                 {
-                    { x => DateTime.TryParse(x.value.ToString(), out _), "Incorrect format of StartTime." },
+                    { x => int.TryParse(x.value.ToString(), out int day) && day < 32 && day > 0, "Incorrect format of Day." }
                 });
 
             #endregion
 
-            #region EndTime
+            #region UserHours
 
             AddFailureForPropertyIf(
-                nameof(EditWorkTimeRequest.EndTime),
+                nameof(EditWorkTimeRequest.UserHours),
                 x => x == OperationType.Replace,
                 new()
                 {
-                    { x => DateTime.TryParse(x.value.ToString(), out _), "Incorrect format of EndTime." },
+                    { x => x.value == null
+                        || float.TryParse(x.value.ToString(), out _), "Incorrect format of UserHours." }
                 });
 
             #endregion
 
-            #region Minutes
+            #region ManagerHours
 
             AddFailureForPropertyIf(
-                nameof(EditWorkTimeRequest.Minutes),
+                nameof(EditWorkTimeRequest.ManagerHours),
                 x => x == OperationType.Replace,
                 new()
                 {
-                    { x => int.TryParse(x.value.ToString(), out _), "Incorrect format of Minutes." },
-                });
-
-            #endregion
-
-            #region IsActive
-
-            AddFailureForPropertyIf(
-                nameof(EditWorkTimeRequest.IsActive),
-                x => x == OperationType.Replace,
-                new()
-                {
-                    { x => bool.TryParse(x.value.ToString(), out _), "Incorrect format of IsActive." },
-                });
-
-            #endregion
-
-            #region ProjectId
-
-            AddFailureForPropertyIf(
-                nameof(EditWorkTimeRequest.IsActive),
-                x => x == OperationType.Replace,
-                new()
-                {
-                    { x => Guid.TryParse(x.value.ToString(), out _), "Incorrect format of ProjectId." },
+                    {
+                        x => x.value == null
+                          || float.TryParse(x.value.ToString(), out _), "Incorrect format of ManagerHours."
+                    }
                 });
 
             #endregion
@@ -105,28 +77,6 @@ namespace LT.DigitalOffice.TimeService.Validation.WorkTime
         {
             RuleForEach(x => x.Operations)
                .Custom(HandleInternalPropertyValidation);
-
-            When(x => x.Operations.Any(o => o.path.EndsWith(nameof(EditWorkTimeRequest.StartTime), StringComparison.OrdinalIgnoreCase))
-                && x.Operations.Any(o => o.path.EndsWith(nameof(EditWorkTimeRequest.EndTime), StringComparison.OrdinalIgnoreCase)),
-                () =>
-                {
-                    RuleFor(x => x)
-                        .Must(x =>
-                        {
-                            DateTime start = DateTime.Parse(x.Operations
-                                .FirstOrDefault(o => o.path.EndsWith(nameof(EditWorkTimeRequest.StartTime), StringComparison.OrdinalIgnoreCase))
-                                .value
-                                .ToString());
-
-                            DateTime end = DateTime.Parse(x.Operations
-                                .FirstOrDefault(o => o.path.EndsWith(nameof(EditWorkTimeRequest.EndTime), StringComparison.OrdinalIgnoreCase))
-                                .value
-                                .ToString());
-
-                            return start < end;
-                        })
-                        .WithMessage("Start time should be less then end.");
-                });
         }
     }
 }
