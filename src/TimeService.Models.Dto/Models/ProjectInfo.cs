6d8c05fe5d3f4a0770f8c56b5d6bfdc31717aac2
BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
2021-09-10 18:39:58 +0300

    update models (#46)
    * update models
    
    * fix solut items
    
    * fix sln

--- src/TimeService.Models.Dto/Models/ProjectInfo.cs
+++ src/TimeService.Models.Dto/Models/ProjectInfo.cs
@@ -8,7 +8,6 @@ namespace LT.DigitalOffice.TimeService.Models.Dto.Models
         public string Name { get; set; }
         public string Status { get; set; }
         public string ShortName { get; set; }
-        public string Description { get; set; }
         public string ShortDescription { get; set; }
     }
 }
