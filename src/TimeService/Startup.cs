611eb274fb4c73e08b0824bdf0bc032e1f4202cd
BelehovEgor <belehov.egor2001@yandex.ru>
2021-11-01 14:45:37 +0300

    update versiom

--- src/TimeService/Startup.cs
+++ src/TimeService/Startup.cs
@@ -56,7 +56,7 @@ namespace LT.DigitalOffice.TimeService
         .GetSection(BaseServiceInfoConfig.SectionName)
         .Get<BaseServiceInfoConfig>();
 
-      Version = "1.1.7.1";
+      Version = "1.1.7.2";
       Description = "TimeService is an API intended to work with the users time managment";
       StartTime = DateTime.UtcNow;
       ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
