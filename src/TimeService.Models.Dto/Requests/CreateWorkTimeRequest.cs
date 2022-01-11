9fc691aa6ea2a30df4552fc588cc744e9b346d88
ndoriomedov <doriomedov213@gmail.com>
2022-01-11 03:39:32 +0300

    update create workTime

--- src/TimeService.Models.Dto/Requests/CreateWorkTimeRequest.cs
+++ src/TimeService.Models.Dto/Requests/CreateWorkTimeRequest.cs
@@ -8,5 +8,6 @@ namespace LT.DigitalOffice.TimeService.Models.Dto.Requests
     public string Description { get; set; }
     public int Year { get; set; }
     public int Month { get; set; }
+    public sbyte Offset { get; set; }
   }
 }
