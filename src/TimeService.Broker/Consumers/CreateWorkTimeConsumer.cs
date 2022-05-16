d1864ab2fb4af7bc3ab29b517e68242031d53b44
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-05-16 16:31:28 +0300

    change consumer to use publish

--- src/TimeService.Broker/Consumers/CreateWorkTimeConsumer.cs
+++ src/TimeService.Broker/Consumers/CreateWorkTimeConsumer.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 using System.Threading.Tasks;
 using LT.DigitalOffice.Kernel.BrokerSupport.Broker;
-using LT.DigitalOffice.Models.Broker.Requests.Time;
+using LT.DigitalOffice.Models.Broker.Publishing.Subscriber.Time;
 using LT.DigitalOffice.TimeService.Data.Interfaces;
 using LT.DigitalOffice.TimeService.Models.Db;
 using MassTransit;
 
 namespace LT.DigitalOffice.TimeService.Broker.Consumers
 {
-  public class CreateWorkTimeConsumer : IConsumer<ICreateWorkTimeRequest>
+  public class CreateWorkTimeConsumer : IConsumer<ICreateWorkTimePublish>
     {
         private readonly IWorkTimeRepository _workTimeRepository;
 
-        private bool CreateWorkTime(ICreateWorkTimeRequest request)
+        private bool CreateWorkTime(ICreateWorkTimePublish publish)
         {
             DateTime timeNow = DateTime.UtcNow;
 
-            foreach(Guid userId in request.UserIds)
+            foreach(Guid userId in publish.UserIds)
             {
                 _workTimeRepository.CreateAsync(
                 new DbWorkTime
                 {
                     Id = Guid.NewGuid(),
-                    ProjectId = request.ProjectId,
+                    ProjectId = publish.ProjectId,
                     UserId = userId,
                     Month = timeNow.Month,
                     Year = timeNow.Year
@@ -37,7 +37,7 @@ namespace LT.DigitalOffice.TimeService.Broker.Consumers
             _workTimeRepository = workTimeRepository;
         }
 
-        public async Task Consume(ConsumeContext<ICreateWorkTimeRequest> context)
+        public async Task Consume(ConsumeContext<ICreateWorkTimePublish> context)
         {
             object result = OperationResultWrapper.CreateResponse(CreateWorkTime, context.Message);
 
