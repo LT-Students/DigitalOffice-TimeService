c9da9f47b5549ff246ad863f2fb30469848f9b3e
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-05-16 16:38:23 +0300

    make CreateWorkTime async

--- src/TimeService.Broker/Consumers/CreateWorkTimeConsumer.cs
+++ src/TimeService.Broker/Consumers/CreateWorkTimeConsumer.cs
@@ -9,39 +9,39 @@ using MassTransit;
 namespace LT.DigitalOffice.TimeService.Broker.Consumers
 {
   public class CreateWorkTimeConsumer : IConsumer<ICreateWorkTimePublish>
+  {
+    private readonly IWorkTimeRepository _workTimeRepository;
+
+    private async Task<bool> CreateWorkTime(ICreateWorkTimePublish publish)
     {
-        private readonly IWorkTimeRepository _workTimeRepository;
+      DateTime timeNow = DateTime.UtcNow;
 
-        private bool CreateWorkTime(ICreateWorkTimePublish publish)
-        {
-            DateTime timeNow = DateTime.UtcNow;
+      foreach (Guid userId in publish.UserIds)
+      {
+        await _workTimeRepository.CreateAsync(
+          new DbWorkTime
+          {
+            Id = Guid.NewGuid(),
+            ProjectId = publish.ProjectId,
+            UserId = userId,
+            Month = timeNow.Month,
+            Year = timeNow.Year
+          });
+      }
 
-            foreach(Guid userId in publish.UserIds)
-            {
-                _workTimeRepository.CreateAsync(
-                new DbWorkTime
-                {
-                    Id = Guid.NewGuid(),
-                    ProjectId = publish.ProjectId,
-                    UserId = userId,
-                    Month = timeNow.Month,
-                    Year = timeNow.Year
-                });
-            }
-
-            return true;
-        }
-
-        public CreateWorkTimeConsumer(IWorkTimeRepository workTimeRepository)
-        {
-            _workTimeRepository = workTimeRepository;
-        }
-
-        public async Task Consume(ConsumeContext<ICreateWorkTimePublish> context)
-        {
-            object result = OperationResultWrapper.CreateResponse(CreateWorkTime, context.Message);
-
-            await context.RespondAsync<IOperationResult<bool>>(result);
-        }
+      return true;
     }
+
+    public CreateWorkTimeConsumer(IWorkTimeRepository workTimeRepository)
+    {
+      _workTimeRepository = workTimeRepository;
+    }
+
+    public async Task Consume(ConsumeContext<ICreateWorkTimePublish> context)
+    {
+      object result = OperationResultWrapper.CreateResponse(CreateWorkTime, context.Message);
+
+      await context.RespondAsync<IOperationResult<bool>>(result);
+    }
+  }
 }
