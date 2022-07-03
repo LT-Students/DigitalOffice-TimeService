fc4bdde9fa75fd98de5d12fcf0030807388f7bf0
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-07-03 23:28:56 +0300

    add services

---
+++ src/TimeService.Broker/Requests/Interfaces/IPositionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Models.Broker.Models.Position;
+
+namespace LT.DigitalOffice.TimeService.Broker.Requests.Interfaces
+{
+  [AutoInject]
+  public interface IPositionService
+  {
+    Task<List<PositionData>> GetPositionsAsync(List<Guid> usersIds, List<string> errors);
+  }
+}
