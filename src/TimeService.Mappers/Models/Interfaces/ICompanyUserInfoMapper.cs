85c2ca07398a19d70717b2d30b2a6638053cad1f
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-07-12 17:59:21 +0300

    Bug/fix services (#97)
    * add endpoints for services
    
    * register request types
    
    * remove whitespaces
    
    * add autoinject
    
    * fix DI
    
    Co-authored-by: Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>

--- src/TimeService.Mappers/Models/Interfaces/ICompanyUserInfoMapper.cs
+++ src/TimeService.Mappers/Models/Interfaces/ICompanyUserInfoMapper.cs
@@ -1,8 +1,10 @@
-﻿using LT.DigitalOffice.Models.Broker.Models.Company;
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Models.Broker.Models.Company;
 using LT.DigitalOffice.TimeService.Models.Dto.Models;
 
 namespace LT.DigitalOffice.TimeService.Mappers.Models.Interfaces
 {
+  [AutoInject]
   public interface ICompanyUserInfoMapper
   {
     CompanyUserInfo Map(CompanyUserData companyUserData);
--- src/TimeService.Mappers/Models/Interfaces/IPositionInfoMapper.cs
+++ src/TimeService.Mappers/Models/Interfaces/IPositionInfoMapper.cs
@@ -1,8 +1,10 @@
-﻿using LT.DigitalOffice.Models.Broker.Models.Position;
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Models.Broker.Models.Position;
 using LT.DigitalOffice.TimeService.Models.Dto.Models;
 
 namespace LT.DigitalOffice.TimeService.Mappers.Models.Interfaces
 {
+  [AutoInject]
   public interface IPositionInfoMapper
   {
     public PositionInfo Map(PositionData positionData);
