7ab6b75736f3cb24570b6f103443cdce6aeb1b41
ndoriomedov <doriomedov213@gmail.com>
2022-07-25 05:24:54 +0300

    update importStat

---
+++ src/TimeService.Models.Dto/Models/UserImportStatInfo.cs
@@ -0,0 +1,11 @@
+﻿using LT.DigitalOffice.Models.Broker.Models;
+using LT.DigitalOffice.Models.Broker.Models.Company;
+
+namespace LT.DigitalOffice.TimeService.Models.Dto.Models
+{
+  public class UserImportStatInfo
+  {
+    public UserData UserData { get; set; }
+    public CompanyUserData CompanyUserData { get; set; }
+  }
+}
