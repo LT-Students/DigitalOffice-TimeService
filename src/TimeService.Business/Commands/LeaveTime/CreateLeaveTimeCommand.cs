2fd00be4c2872474a47812f20d4b5a996e0ed9a9
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-08-18 18:05:48 +0300

    Feature/add unit tests (#110)

--- src/TimeService.Business/Commands/LeaveTime/CreateLeaveTimeCommand.cs
+++ src/TimeService.Business/Commands/LeaveTime/CreateLeaveTimeCommand.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using FluentValidation.Results;
 using LT.DigitalOffice.Kernel.BrokerSupport.AccessValidatorEngine.Interfaces;
 using LT.DigitalOffice.Kernel.Constants;
 using LT.DigitalOffice.Kernel.Extensions;
-using LT.DigitalOffice.Kernel.FluentValidationExtensions;
+using LT.DigitalOffice.Kernel.Helpers.Interfaces;
 using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.TimeService.Business.Commands.LeaveTime.Interfaces;
 using LT.DigitalOffice.TimeService.Data.Interfaces;
@@ -23,54 +24,48 @@ namespace LT.DigitalOffice.TimeService.Business.Commands.LeaveTime
     private readonly ILeaveTimeRepository _repository;
     private readonly IAccessValidator _accessValidator;
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IResponseCreator _responseCreator;
 
     public CreateLeaveTimeCommand(
       ICreateLeaveTimeRequestValidator validator,
       IDbLeaveTimeMapper mapper,
       ILeaveTimeRepository repository,
       IAccessValidator accessValidator,
-      IHttpContextAccessor httpContextAccessor)
+      IHttpContextAccessor httpContextAccessor,
+      IResponseCreator responseCreator)
     {
       _validator = validator;
       _mapper = mapper;
       _repository = repository;
       _accessValidator = accessValidator;
       _httpContextAccessor = httpContextAccessor;
+      _responseCreator = responseCreator;
     }
 
     public async Task<OperationResultResponse<Guid?>> ExecuteAsync(CreateLeaveTimeRequest request)
     {
-      var isOwner = request.UserId == _httpContextAccessor.HttpContext.GetUserId();
-
+      bool isOwner = request.UserId == _httpContextAccessor.HttpContext.GetUserId();
       if (!isOwner && !await _accessValidator.HasRightsAsync(Rights.AddEditRemoveTime))
       {
-        _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
-
-        return new();
+        return _responseCreator.CreateFailureResponse<Guid?>(HttpStatusCode.Forbidden);
       }
 
-      if (!_validator.ValidateCustom(request, out List<string> errors))
+      ValidationResult validationResult = await _validator.ValidateAsync(request);
+      if (!validationResult.IsValid)
       {
-        _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-
-        return new()
-        {
-          Errors = errors
-        };
+        return _responseCreator.CreateFailureResponse<Guid?>(
+          HttpStatusCode.BadRequest,
+          validationResult.Errors.Select(e => e.ErrorMessage).ToList());
       }
 
       OperationResultResponse<Guid?> response = new();
-
       response.Body = await _repository.CreateAsync(_mapper.Map(request));
 
       _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Created;
 
-      if (response.Body == default)
-      {
-        _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-      }
-
-      return response;
+      return response.Body is null
+        ? _responseCreator.CreateFailureResponse<Guid?>(HttpStatusCode.BadRequest)
+        : response;
     }
   }
 }
--- test/TimeService.Business.UnitTests/Commands/LeaveTime/CreateLeaveTimeCommandTests.cs
+++ test/TimeService.Business.UnitTests/Commands/LeaveTime/CreateLeaveTimeCommandTests.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 using LT.DigitalOffice.Kernel.BrokerSupport.AccessValidatorEngine.Interfaces;
+using LT.DigitalOffice.Kernel.Constants;
+using LT.DigitalOffice.Kernel.Helpers.Interfaces;
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.TimeService.Business.Commands.LeaveTime;
 using LT.DigitalOffice.TimeService.Business.Commands.LeaveTime.Interfaces;
 using LT.DigitalOffice.TimeService.Data.Interfaces;
 using LT.DigitalOffice.TimeService.Mappers.Db.Interfaces;
@@ -7,149 +14,211 @@ using LT.DigitalOffice.TimeService.Models.Db;
 using LT.DigitalOffice.TimeService.Models.Dto.Enums;
 using LT.DigitalOffice.TimeService.Models.Dto.Requests;
 using LT.DigitalOffice.TimeService.Validation.LeaveTime.Interfaces;
+using LT.DigitalOffice.UnitTestKernel;
 using Microsoft.AspNetCore.Http;
 using Moq;
+using Moq.AutoMock;
 using NUnit.Framework;
 
 namespace LT.DigitalOffice.TimeService.Business.UnitTests.Commands.LeaveTime
 {
   public class CreateLeaveTimeCommandTests
+  {
+    private AutoMocker _mocker;
+    private ICreateLeaveTimeCommand _command;
+
+    private CreateLeaveTimeRequest _request;
+    private DbLeaveTime _createdLeaveTime;
+    private OperationResultResponse<Guid?> _badResponse;
+    private OperationResultResponse<Guid?> _goodResponse;
+    private Guid _createdBy;
+    private Dictionary<object, object> _items;
+
+    private void Verifiable(
+      Times accessValidatorTimes,
+      Times validatorTimes,
+      Times responseCreatorTimes,
+      Times repositoryTimes,
+      Times mapperTimes,
+      Times httpContextAccessorItemsTimes)
     {
-        private Mock<ICreateLeaveTimeRequestValidator> _validatorMock;
-        private Mock<IDbLeaveTimeMapper> _mapperMock;
-        private Mock<ILeaveTimeRepository> _repositoryMock;
-        private Mock<IAccessValidator> _accessValidatorMock;
-        private Mock<IHttpContextAccessor> _httpContextAccessorMock;
-        private ICreateLeaveTimeCommand _command;
+      _mocker.Verify<IAccessValidator>(x =>
+        x.HasRightsAsync(Rights.AddEditRemoveTime),
+        accessValidatorTimes);
 
-        private CreateLeaveTimeRequest _request;
-        private DbLeaveTime _createdLeaveTime;
-        private Guid _createdBy;
+      _mocker.Verify<ICreateLeaveTimeRequestValidator, bool>(x =>
+          x.ValidateAsync(_request, default).Result.IsValid,
+        validatorTimes);
 
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            _createdBy = Guid.NewGuid();
+      _mocker.Verify<IResponseCreator>(x =>
+        x.CreateFailureResponse<Guid?>(It.IsAny<HttpStatusCode>(), It.IsAny<List<string>>()),
+        responseCreatorTimes);
 
-            _request = new CreateLeaveTimeRequest()
-            {
-                LeaveType = LeaveType.SickLeave,
-                Comment = "I have a sore throat",
-                StartTime = new DateTime(2020, 7, 24),
-                EndTime = new DateTime(2020, 7, 27),
-                UserId = _createdBy
-            };
+      _mocker.Verify<ILeaveTimeRepository>(x =>
+          x.CreateAsync(It.IsAny<DbLeaveTime>()),
+        repositoryTimes);
 
-            _createdLeaveTime = new DbLeaveTime()
-            {
-                Id = Guid.NewGuid(),
-                CreatedBy = _createdBy,
-                LeaveType = (int)_request.LeaveType,
-                Comment = _request.Comment,
-                StartTime = _request.StartTime.UtcDateTime,
-                EndTime = _request.EndTime.UtcDateTime,
-                UserId = _createdBy
-            };
-        }
+      _mocker.Verify<IDbLeaveTimeMapper>(x =>
+          x.Map(It.IsAny<CreateLeaveTimeRequest>()),
+        mapperTimes);
 
-        /*[SetUp]
-        public void SetUp()
-        {
-            _validatorMock = new Mock<ICreateLeaveTimeRequestValidator>();
-            _mapperMock = new Mock<IDbLeaveTimeMapper>();
-            _repositoryMock = new Mock<ILeaveTimeRepository>();
-            _accessValidatorMock = new Mock<IAccessValidator>();
-            _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+      _mocker.Verify<IHttpContextAccessor>(x =>
+        x.HttpContext.Items,
+        httpContextAccessorItemsTimes);
 
-            var items = new Dictionary<object, object>
-            {
-                { "UserId", _createdBy }
-            };
-
-            _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
-            _httpContextAccessorMock
-                .Setup(x => x.HttpContext.Items)
-                .Returns(items);
-
-            _accessValidatorMock
-                .Setup(x => x.IsAdmin(null))
-                .Returns(false);
-
-            _command = new CreateLeaveTimeCommand(_validatorMock.Object, _mapperMock.Object, _repositoryMock.Object, _accessValidatorMock.Object, _httpContextAccessorMock.Object);
-        }*/
-
-        /*[Test]
-        public void ShouldThrowExceptionWhenUserCreateLeaveTimeToOtherUserAndHisIsNotAdmin()
-        {
-            _accessValidatorMock
-                .Setup(x => x.IsAdmin(null))
-                .Returns(false);
-
-            var items = new Dictionary<object, object>
-            {
-                { "UserId", Guid.NewGuid() }
-            };
-
-            _httpContextAccessorMock
-                .Setup(x => x.HttpContext.Items)
-                .Returns(items);
-
-            Assert.Throws<ForbiddenException>(() => _command.Execute(_request));
-            _repositoryMock.Verify(repository => repository.Add(It.IsAny<DbLeaveTime>()), Times.Never);
-        }*/
-
-        //[Test]
-        //public void ShouldThrowExceptionWhenValidatorThrowsException()
-        //{
-        //    _validatorMock
-        //        .Setup(x => x.Validate(It.IsAny<IValidationContext>()).IsValid)
-        //        .Returns(false);
-
-        //    Assert.Throws<ValidationException>(() => _command.Execute(_request));
-        //    _repositoryMock.Verify(repository => repository.Add(It.IsAny<DbLeaveTime>()), Times.Never);
-        //}
-
-        /*[Test]
-        public void ShouldThrowExceptionWhenRepositoryThrowsException()
-        {
-            _validatorMock
-                 .Setup(x => x.Validate(It.IsAny<IValidationContext>()).IsValid)
-                 .Returns(true);
-
-            _mapperMock
-                .Setup(x => x.Map(It.IsAny<CreateLeaveTimeRequest>(), _createdBy))
-                .Returns(_createdLeaveTime);
-
-            _repositoryMock
-                .Setup(x => x.Add(It.IsAny<DbLeaveTime>()))
-                .Throws(new Exception());
-
-            Assert.Throws<Exception>(() => _command.ExecuteAsync(_request));
-        }*/
-
-        /*[Test]
-        public void ShouldCreateNewLeaveTimeWhenDataIsValid()
-        {
-            _validatorMock
-                 .Setup(x => x.Validate(It.IsAny<IValidationContext>()).IsValid)
-                 .Returns(true);
-
-            _mapperMock
-                .Setup(x => x.Map(It.IsAny<CreateLeaveTimeRequest>(), _createdBy))
-                .Returns(_createdLeaveTime);
-
-            _repositoryMock
-                .Setup(x => x.Add(It.IsAny<DbLeaveTime>()))
-                .Returns(_createdLeaveTime.Id);
-
-            var expected = new OperationResultResponse<Guid>
-            {
-                Body = _createdLeaveTime.Id,
-                Status = OperationResultStatusType.FullSuccess
-            };
-
-            SerializerAssert.AreEqual(expected, _command.ExecuteAsync(_request));
-            _repositoryMock.Verify(repository => repository.Add(It.IsAny<DbLeaveTime>()), Times.Once);
-        }*/
+      _mocker.Resolvers.Clear();
     }
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+      _createdBy = Guid.NewGuid();
+
+      _request = new CreateLeaveTimeRequest
+      {
+        LeaveType = LeaveType.SickLeave,
+        Comment = "I have a sore throat",
+        StartTime = new DateTime(2020, 7, 24),
+        EndTime = new DateTime(2020, 7, 27),
+        UserId = _createdBy
+      };
+
+      _createdLeaveTime = new DbLeaveTime
+      {
+        Id = Guid.NewGuid(),
+        CreatedBy = _createdBy,
+        LeaveType = (int)_request.LeaveType,
+        Comment = _request.Comment,
+        StartTime = _request.StartTime.UtcDateTime,
+        EndTime = _request.EndTime.UtcDateTime,
+        UserId = _createdBy
+      };
+
+      _badResponse = new()
+      {
+        Body = null,
+        Errors = new List<string> { "Error" }
+      };
+
+      _goodResponse = new()
+      {
+        Body = _createdLeaveTime.Id,
+        Errors = new List<string>()
+      };
+
+      _items = new()
+      {
+        { "UserId", _createdBy }
+      };
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+      _mocker = new AutoMocker();
+      _command = _mocker.CreateInstance<CreateLeaveTimeCommand>();
+
+      _mocker.Setup<IAccessValidator, Task<bool>>(x =>
+          x.HasRightsAsync(Rights.AddEditRemoveTime))
+        .ReturnsAsync(true);
+
+      _mocker.Setup<ICreateLeaveTimeRequestValidator, bool>(x =>
+          x.ValidateAsync(_request,default).Result.IsValid)
+        .Returns(true);
+
+      _mocker.Setup<IResponseCreator, OperationResultResponse<Guid?>>(x =>
+          x.CreateFailureResponse<Guid?>(It.IsAny<HttpStatusCode>(), It.IsAny<List<string>>()))
+        .Returns(_badResponse);
+
+      _mocker.Setup<ILeaveTimeRepository, Task<Guid?>>(x =>
+          x.CreateAsync(It.IsAny<DbLeaveTime>()))
+        .ReturnsAsync(_createdLeaveTime.Id);
+
+      _mocker.Setup<IDbLeaveTimeMapper, DbLeaveTime>(x =>
+          x.Map(It.IsAny<CreateLeaveTimeRequest>()))
+        .Returns(_createdLeaveTime);
+
+      _mocker.Setup<IHttpContextAccessor, IDictionary<object, object>>(x =>
+          x.HttpContext.Items)
+        .Returns(_items);
+      _mocker.Setup<IHttpContextAccessor, int>(x =>
+          x.HttpContext.Response.StatusCode)
+        .Returns((int)HttpStatusCode.Created);
+    }
+
+    [Test]
+    public async Task FailUserCheckAsync()
+    {
+      Dictionary<object, object> items = new()
+      {
+        { "UserId", Guid.NewGuid() }
+      };
+      _mocker.Setup<IHttpContextAccessor, IDictionary<object, object>>(x =>
+          x.HttpContext.Items)
+        .Returns(items);
+      _mocker.Setup<IAccessValidator, Task<bool>>(x =>
+          x.HasRightsAsync(Rights.AddEditRemoveTime))
+        .ReturnsAsync(false);
+
+      SerializerAssert.AreEqual(_badResponse, await _command.ExecuteAsync(_request));
+
+      Verifiable(
+        Times.Once(),
+        Times.Never(),
+        Times.Once(),
+        Times.Never(),
+        Times.Never(),
+        Times.Exactly(2));
+    }
+
+    [Test]
+    public async Task FailValidationAsync()
+    {
+      _mocker.Setup<ICreateLeaveTimeRequestValidator, bool>(x =>
+          x.ValidateAsync(_request, default).Result.IsValid)
+        .Returns(false);
+
+      SerializerAssert.AreEqual(_badResponse, await _command.ExecuteAsync(_request));
+
+      Verifiable(
+        Times.Never(),
+        Times.Once(),
+        Times.Once(),
+        Times.Never(),
+        Times.Never(),
+        Times.Exactly(2));
+    }
+
+    [Test]
+    public async Task FailRepositoryCreateAsync()
+    {
+      _mocker.Setup<ILeaveTimeRepository, Task<Guid?>>(x =>
+          x.CreateAsync(It.IsAny<DbLeaveTime>()))
+        .ReturnsAsync((Guid?)null);
+
+      SerializerAssert.AreEqual(_badResponse, await _command.ExecuteAsync(_request));
+
+      Verifiable(
+        Times.Never(),
+        Times.Once(),
+        Times.Once(),
+        Times.Once(),
+        Times.Once(),
+        Times.Exactly(2));
+    }
+
+    [Test]
+    public async Task CreateLeaveTimeSuccessfullyAsync()
+    {
+      SerializerAssert.AreEqual(_goodResponse, await _command.ExecuteAsync(_request));
+
+      Verifiable(
+        Times.Never(),
+        Times.Once(),
+        Times.Never(),
+        Times.Once(),
+        Times.Once(),
+        Times.Exactly(2));
+    }
+  }
 }
