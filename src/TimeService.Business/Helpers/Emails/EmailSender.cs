5a81ea112383001311c5b72f503dc8459a625518
VLiamin <43515136+VLiamin@users.noreply.github.com>
2022-10-05 14:15:12 +0300

    Changed time (#137)
    * changed time
    
    * small fixe

--- src/TimeService.Business/Helpers/Emails/EmailSender.cs
+++ src/TimeService.Business/Helpers/Emails/EmailSender.cs
@@ -99,7 +99,8 @@ namespace LT.DigitalOffice.TimeService.Business.Helpers.Emails
       {
         while (true)
         {
-          if (DateTime.UtcNow.Day == DateTime.DaysInMonth(DateTime.UtcNow.Year, month: DateTime.UtcNow.Month) - 1 && DateTime.UtcNow.Hour == 0)
+         // if (DateTime.UtcNow.Day == DateTime.DaysInMonth(DateTime.UtcNow.Year, month: DateTime.UtcNow.Month) - 1 && DateTime.UtcNow.Hour == 0)
+            if (DateTime.UtcNow.Day == 5 && DateTime.UtcNow.Hour == 13)
           {
             await ExecuteAsync();
           }
